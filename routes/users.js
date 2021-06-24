970c9dffe2bb2251dc03243847eafc1643d905ad
unfoldedRoses <ptest5557@gmail.com>
2021-06-24 20:53:38 +0530

    added models routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var {postRoute}=require('../controllers/index')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', postRoute
+// );
+
+router.post('/', postRoute
+);
+
 
 module.exports = router;
