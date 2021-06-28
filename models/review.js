1049d56d151191e695b8bf6b2eb7763d638e864c
unfoldedRoses <ptest5557@gmail.com>
2021-06-28 16:25:30 +0530

    mapbox working with layouts-some views missing

--- models/review.js
+++ models/review.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-	body: String,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
-	}
+  body: String,
+  rating: Number,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-module.exports = mongoose.model('Review', ReviewSchema);
+module.exports = mongoose.model("Review", ReviewSchema);
